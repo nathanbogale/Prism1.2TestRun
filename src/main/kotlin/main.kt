0ceaf15124c75ce11a6642f561e18e95b98720ad
Nathan Bogale <nathanbogale@gmail.com>
2021-10-29 15:42:49 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+import java.sql.DriverManager.println
+import io.iohk.atala.prism.kotlin.crypto.*
+import io.iohk.atala.prism.kotlin.identity.*
+import io.iohk.atala.prism.kotlin.credentials.*
+import io.iohk.atala.prism.kotlin.credentials.content.*
+import io.iohk.atala.prism.kotlin.credentials.json.*
+import kotlinx.datetime.*
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+
+
+fun main() {
+    println("Hello Prism")
+}
